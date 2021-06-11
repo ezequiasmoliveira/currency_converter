b54e77fd4c51ba2a48df2f5696b283f027a0f29e
esequias.m.oliveira <esequias.m.oliveira@gmail.com>
2021-06-10 23:33:35 -0300

    Add home view

---
+++ lib/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:currency_converter/app/components/currency_box.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 30, right: 30, top: 100, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(height: 50),
+              CurrencyBox(),
+              SizedBox(height: 10),
+              CurrencyBox(),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(primary: Colors.amber),
+                child: Text('CONVERTER'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
